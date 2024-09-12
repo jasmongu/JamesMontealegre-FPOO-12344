033cc31cd71da922de255b1741ef2fdf218a9bcc
James Montealegre <james.montealegre.g@uniminuto.edu.co>
2024-09-12 01:17:15 +0000

    This a basic program to provide a name and user age

---
+++ SemanaUno/Lab-1.py
@@ -0,0 +1,8 @@
+def get_average_and_largest():
+    name = input("Ingrese su nombre: ")
+    age  = int(input("Ingrese su edad: "))
+
+    print(f"Me llamo {name} y tengo {age}")
+
+get_average_and_largest()
+
